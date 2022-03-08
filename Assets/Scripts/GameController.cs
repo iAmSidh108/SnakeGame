d8fcab494d2bcf1ec291cd3efb8d96c88608dd0a
Siddharth Tiwari <sidh589@gmail.com>
2022-03-08 19:34:18 +0530

    Setup Basic Elements for Android Snake Game
    Changed Orientation to 1080*1920 portrait.
    Changed build setting to Android.
    Imported all the assets required for the game.
    Created a basic gameController and added a script.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
