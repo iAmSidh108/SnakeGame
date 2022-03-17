cda3f2e3a40aada1a82853e832d3d48b8c86862f
Siddharth Tiwari <sidh589@gmail.com>
2022-03-17 20:33:46 +0530

    Created walls around height and width
    Instantiated rock prefab then combined them to form a wall then increased the number of wall to 4 as wall of 4 sides.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public static GameController instance = null;
+
+    const float width = 4f;
+    const float height = 7f;
+
     public float snakeSpeed = 1;
 
     public BodyPart bodyPrefab = null;
+    public GameObject rockPrefab = null;
 
     public Sprite tailSprite = null;
     public Sprite bodySprite = null;
@@ -17,6 +22,7 @@ public class GameController : MonoBehaviour
     {
         instance = this;
         Debug.Log("Starting the Snake Game");
+        CreateWalls();
         StartGame();
     }
 
@@ -29,5 +35,45 @@ public class GameController : MonoBehaviour
     void StartGame()
     {
         snakeHead.ResetSnake();
+    }
+    void CreateWalls()
+    {
+        Vector3 start = new Vector3(-width, -height, 0);   //left wall
+        Vector3 finish = new Vector3(-width, +height, 0);
+        CreateWall(start, finish);
+
+         start = new Vector3(+width, -height, 0);         // right wall
+         finish = new Vector3(+width, +height, 0);
+         CreateWall(start, finish);
+
+         start = new Vector3(-width, +height, 0);         //  Up wall
+         finish = new Vector3(+width, +height, 0);
+         CreateWall(start, finish);
+
+         start = new Vector3(-width, -height, 0);         //down wall
+         finish = new Vector3(+width, -height, 0);
+         CreateWall(start, finish);
+
+    }
+    void CreateWall(Vector3 start, Vector3 finish)
+    {
+        float distance = Vector3.Distance(start, finish);
+        int noOfRocks = (int)(distance * 3f);
+        Vector3 delta = (finish - start) / noOfRocks;
+
+        Vector3 position = start;
+        for(int rock = 0; rock <= noOfRocks; rock++)
+        {
+            float rotation = Random.Range(0, 360f);
+            float scale = Random.Range(1f, 2f);
+            CreateRock(position, scale, rotation);
+            position = position + delta;
+        }
+    }
+
+    void CreateRock(Vector3 position, float scale,float rotation)
+    {
+        GameObject rock = Instantiate(rockPrefab,position,Quaternion.Euler(0,0,rotation));
+        rock.transform.localScale = new Vector3(scale, scale, 0);
     }
 }
